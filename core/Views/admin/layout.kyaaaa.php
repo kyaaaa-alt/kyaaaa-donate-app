c18249aef35007c0eeefe32250a391f944ff0417
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-30 08:02:41 +0700

    Create layout.kyaaaa.php

---
+++ core/Views/admin/layout.kyaaaa.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title; ?></title>
+    <link href="<?= url('assets/home/css') ?>/custom.css" rel="stylesheet">
+</head>
+<body>
+    <?= $content; ?>
+<script src="<?= url('assets/home/js') ?>/custom.js"></script>
+</body>
+</html>
