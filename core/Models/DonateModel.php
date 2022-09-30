da85db81b931f8c0d4a9c8a2ddb720a6f2e00785
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-30 10:51:11 +0700

    Create DonateModel.php

---
+++ core/Models/DonateModel.php
@@ -0,0 +1,17 @@
+<?php namespace Core\Models;
+
+use Core\Conf\Kyaaaa\DB;
+
+class DonateModel {
+    public function __construct() {
+        $this->users = DB::query('users');
+    }
+
+    public function get_username() {
+        $builder = $this->users;
+        $builder->select('username');
+        $query = $builder->get()[0]->username;
+        return $query;
+    }
+
+}
