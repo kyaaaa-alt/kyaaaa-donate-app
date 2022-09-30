3cc1b8f5ff61daeca395e59d5b8c0f658f495905
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-09-30 10:51:03 +0700

    Update layout.kyaaaa.php

--- core/Views/home/layout.kyaaaa.php
+++ core/Views/home/layout.kyaaaa.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
-    <link href="<?= url('assets/home/css') ?>/custom.css" rel="stylesheet">
-    <link href="<?= url('assets/home/css') ?>/sweetalert2.dark.css" rel="stylesheet">
+    <link href="<?= url('assets/css') ?>/custom.css" rel="stylesheet">
+
 </head>
 <body>
     <?= $content; ?>
-<script src="<?= url('assets/home/js') ?>/custom.js"></script>
-<script src="<?= url('assets/home/js') ?>/sweetalert2.all.min.js"></script>
+<script src="<?= url('assets/js') ?>/custom.js"></script>
+<script src="<?= url('assets/js') ?>/sweetalert2.all.min.js"></script>
 </body>
 </html>
