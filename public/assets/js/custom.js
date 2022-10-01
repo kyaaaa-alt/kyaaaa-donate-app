7669e53b22096e47db3d66bd027b072cf416d9ca
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 17:42:02 +0700

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -10,7 +10,7 @@ for (var i = 0; i < amountBtn.length; i++) {
         this.setAttribute('checked', '');
         document.querySelector("button").innerHTML = 'Donate Rp ' + document.querySelector('.selected input').value
         const amount = document.querySelector('.selected input').value.replace(/[^0-9]/g, '');
-        // document.querySelector("#amount").value = amount;
+        document.querySelector("#amount").value = amount;
         // document.querySelector("#custom_amount").value = document.querySelector('.selected input').value;
     });
 }
@@ -20,6 +20,8 @@ amount.addEventListener('focus', function (event) {
     document.querySelector('.selected').removeAttribute("checked");
     document.querySelector('.selected').classList.remove("selected");
     amount.classList.add("selected");
+    document.querySelector("#amount").value = '';
+    document.querySelector("button").innerHTML = 'Donate Rp ';
 });
 
 amount.addEventListener('keyup', function (event) {
