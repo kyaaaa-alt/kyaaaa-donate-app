7fe0b82900408dd3b2dd7d6c9966f2bc1fd499f6
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 21:17:30 +0700

    Create auth.kyaaaa.php

---
+++ core/Views/auth/auth.kyaaaa.php
@@ -0,0 +1,34 @@
+<div class="donate mt-20">
+    <div class="donate-black text-center">
+        <h3 class="mb-n5 mt-n3">LOGIN</h3>
+    </div>
+    <div class="donate-blue">
+        <div class="donate-amount-box">
+
+            <?php $session = session();
+                if ($session->has('notif')) {
+            ?>
+            <div class="text-center mb-25" id="hideMe">
+                <span class="alert"><?= $session->getFlash('notif'); ?></span>
+            </div>
+            <?php } ?>
+
+            <div class="donate-amount">
+                <form action="<?= url('auth') ?>" method="post">
+                    <?php csrf()->tokenField(); ?>
+                    <div class="input-group">
+                        <input autocomplete="off" type="email"  name="email" value="" placeholder="Email">
+                    </div>
+                    <div class="input-group">
+                        <input autocomplete="off" type="password"  name="password" value="" placeholder="Password">
+                    </div>
+                    <div class="donate-submit">
+                        <button type="submit" autocomplete="off">LOGIN</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+</div>
