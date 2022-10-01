75f161380b2b1bd07f9e152c2bf49104b16700be
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-02 00:49:52 +0700

    Update settings.js

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -8,3 +8,41 @@ fileInput.onchange = function() {
     let selectName = document.getElementsByClassName("file-select-name")[0];
     selectName.innerText = filename;
 }
+
+let fileInput2 = document.getElementById("file-upload-input2");
+let fileSelect2 = document.getElementsByClassName("file-upload-select2")[0];
+fileSelect2.onclick = function() {
+    fileInput2.click();
+}
+fileInput2.onchange = function() {
+    let filename2 = fileInput2.files[0].name;
+    let selectName2 = document.getElementsByClassName("file-select-name2")[0];
+    selectName2.innerText = filename2;
+}
+
+function showProfile() {
+    document.getElementById('profile').style.display = 'initial';
+    document.getElementById('stream').style.display = 'none';
+    document.getElementById('tripay').style.display = 'none';
+    document.getElementById('pusher').style.display = 'none';
+}
+function showStream() {
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('stream').style.display = 'initial';
+    document.getElementById('tripay').style.display = 'none';
+    document.getElementById('pusher').style.display = 'none';
+}
+
+function showTripay() {
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('stream').style.display = 'none';
+    document.getElementById('tripay').style.display = 'initial';
+    document.getElementById('pusher').style.display = 'none';
+}
+
+function showPusher() {
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('stream').style.display = 'none';
+    document.getElementById('tripay').style.display = 'none';
+    document.getElementById('pusher').style.display = 'initial';
+}
