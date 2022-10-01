0c99b76ee39840157e0db0a39b8d3208415ee0f9
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 17:42:17 +0700

    Update CSRF.php

--- core/Filter/CSRF.php
+++ core/Filter/CSRF.php
@@ -1,7 +1,14 @@
-<?php namespace Core\Conf\Filter;
+<?php namespace Core\Filter;
 
 class CSRF {
     public function donate() {
-        
+        if(!csrf()->isValidRequest()){
+            echo json_encode([
+                    'success' => false,
+                    'invoice' => '',
+                    'msg' => 'Invalid token: reload this page (donation only accept from orign webform) not http clients']
+            );
+            die;
+        }
     }
 }
