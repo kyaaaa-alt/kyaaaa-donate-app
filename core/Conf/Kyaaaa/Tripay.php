732cb6d66fc6e8a196c444b662474d63b032fbeb
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 12:49:31 +0700

    Update Tripay.php

--- core/Conf/Kyaaaa/Tripay.php
+++ core/Conf/Kyaaaa/Tripay.php
@@ -4,12 +4,11 @@ use Core\Conf\Kyaaaa\DB;
 
 class Tripay {
     public function __construct() {
-        $builder = DB::query('settings')->select('*');
-        $settings = $builder->get();
-        $this->merchantCode = $settings[0]->merchant_code;
-        $this->apiKey = $settings[0]->merchant_api_key;
-        $this->privateKey = $settings[0]->merchant_private_key;
-        $this->endpoint = $settings[0]->endpoint;
+        $tripay_api_key = DB::query('settings')->select('*')->get();
+        $this->merchantCode = $tripay_api_key[0]->merchant_code;
+        $this->apiKey = $tripay_api_key[0]->merchant_api_key;
+        $this->privateKey = $tripay_api_key[0]->merchant_private_key;
+        $this->endpoint = $tripay_api_key[0]->endpoint;
     }
 
     public function payment_channel() {
@@ -48,41 +47,42 @@ class Tripay {
         }
     }
 
-    public function request_payment($payload) {
-        /* @ ContohPayload
-        $payload = [
-        'payment_method' => '',
-        'name' => '',
-        'email' => '',
-        'phone' => '',
-        'amount' => 'total price',
-        'order_items' => [
-        [
-        'name'        => 'Nama Produk 1',
-        'price'       => 500000,
-        'quantity'    => 1,
-        ]
-        ],
-        ];
-         */
-        // $payload = json_decode($payload);
-        date_default_timezone_set('Asia/Jakarta');
-        $today = date('dmyHis');
 
+    /** @ ContohPayload
+    * @array $payload = [
+    *           'name' => '',
+    *           'email' => '',
+    *           'phone' => '',
+    *           'amount' => 'total price',
+    *       ];
+    **/
+    // $payload = json_decode($payload);
+    public function request_payment($payload) {
+
+        $today = date('dmyHis');
         $apiKey       = $this->apiKey;
         $privateKey   = $this->privateKey;
         $merchantCode = $this->merchantCode;
-        $merchantRef  = 'INVD/'. substr($payload['phone'], -3) . '/' . $today;
+        $phone = '0812' . rand(11111111,99999999);
+        $merchantRef  = 'INVD/'. substr($payload['name'], -3) . '/' . $today;
+        $items = [
+            [
+                'sku' => 'TF',
+                'name'        => 'from ' . $payload['name'],
+                'price'       => $payload['amount'],
+                'quantity'    => 1,
+            ]
+        ];
 
         $data = [
-            'method'         => $payload['payment_method'],
+            'method'         => 'QRISC',
             'merchant_ref'   => $merchantRef,
             'amount'         => $payload['amount'],
             'customer_name'  => $payload['name'],
             'customer_email' => $payload['email'],
-            'customer_phone' => $payload['phone'],
-            'order_items'    => $payload['order_items'],
-            'return_url'   => base_url(),
+            'customer_phone' => $phone,
+            'order_items'    => $items,
+            'return_url'   => url(),
             'expired_time' => (time() + (24 * 60 * 60)),
             'signature'    => hash_hmac('sha256', $merchantCode.$merchantRef.$payload['amount'], $privateKey)
         ];
@@ -101,9 +101,76 @@ class Tripay {
         ]);
 
         $response = json_decode(curl_exec($curl));
-        $error = json_decode(curl_error($curl));
         curl_close($curl);
-        return $response;
+
+        if ($response->success == true) {
+            return $response;
+        } else {
+            $data2 = [
+                'method'         => 'QRIS',
+                'merchant_ref'   => $merchantRef,
+                'amount'         => $payload['amount'],
+                'customer_name'  => $payload['name'],
+                'customer_email' => $payload['email'],
+                'customer_phone' => $phone,
+                'order_items'    => $items,
+                'return_url'   => url(),
+                'expired_time' => (time() + (24 * 60 * 60)),
+                'signature'    => hash_hmac('sha256', $merchantCode.$merchantRef.$payload['amount'], $privateKey)
+            ];
+
+            $curl2 = curl_init();
+            curl_setopt_array($curl2, [
+                CURLOPT_FRESH_CONNECT  => true,
+                CURLOPT_URL            => 'https://tripay.co.id/'.$this->endpoint.'/transaction/create',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER         => false,
+                CURLOPT_HTTPHEADER     => ['Authorization: Bearer '.$apiKey],
+                CURLOPT_FAILONERROR    => false,
+                CURLOPT_POST           => true,
+                CURLOPT_POSTFIELDS     => http_build_query($data2),
+                CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+            ]);
+
+            $response2 = json_decode(curl_exec($curl2));
+            curl_close($curl2);
+
+            if ($response2->success == true){
+                return $response2;
+            } else {
+                $data3 = [
+                    'method'         => 'QRIS2',
+                    'merchant_ref'   => $merchantRef,
+                    'amount'         => $payload['amount'],
+                    'customer_name'  => $payload['name'],
+                    'customer_email' => $payload['email'],
+                    'customer_phone' => $phone,
+                    'order_items'    => $items,
+                    'return_url'   => url(),
+                    'expired_time' => (time() + (24 * 60 * 60)),
+                    'signature'    => hash_hmac('sha256', $merchantCode.$merchantRef.$payload['amount'], $privateKey)
+                ];
+
+                $curl3 = curl_init();
+                curl_setopt_array($curl3, [
+                    CURLOPT_FRESH_CONNECT  => true,
+                    CURLOPT_URL            => 'https://tripay.co.id/'.$this->endpoint.'/transaction/create',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_HEADER         => false,
+                    CURLOPT_HTTPHEADER     => ['Authorization: Bearer '.$apiKey],
+                    CURLOPT_FAILONERROR    => false,
+                    CURLOPT_POST           => true,
+                    CURLOPT_POSTFIELDS     => http_build_query($data3),
+                    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+                ]);
+
+                $response3 = json_decode(curl_exec($curl3));
+                curl_close($curl3);
+
+                return $response3;
+            }
+        }
+
     }
 
     public function invoice($reference) {
