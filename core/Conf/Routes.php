15874419a0eb319c521120406e26537c4735ea3c
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 21:17:46 +0700

    Update Routes.php

--- core/Conf/Routes.php
+++ core/Conf/Routes.php
@@ -7,13 +7,19 @@ class Routes {
         $router = new Router();
 
         $router->get('/', [\Core\Controllers\DonateCtrl::class,'index']);
-
         $router->post('/do_donate', [\Core\Controllers\PaymentCtrl::class,'do_donate'])
             ->middleware('\Core\Filter\CSRF#donate');
-
         $router->get('/inv/:id', [\Core\Controllers\PaymentCtrl::class,'invoice']);
 
         $router->get('/login', [\Core\Controllers\AdminCtrl::class,'login']);
+        $router->get('/logout', [\Core\Controllers\AdminCtrl::class,'logout']);
+        $router->post('/auth', [\Core\Controllers\AdminCtrl::class,'auth'])
+            ->middleware('\Core\Filter\CSRF#auth');
+
+        $router->get('/dashboard', [\Core\Controllers\AdminCtrl::class,'index'])
+            ->middleware('\Core\Filter\Auth#admin');
+        $router->get('/dashboard/settings', [\Core\Controllers\AdminCtrl::class,'settings'])
+            ->middleware('\Core\Filter\Auth#admin');
 
 
         $router->run();
