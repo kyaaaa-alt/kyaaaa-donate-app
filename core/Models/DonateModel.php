c7747e9c91075b48c8b84c475b574142c18b3ff4
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 12:49:17 +0700

    Update DonateModel.php

--- core/Models/DonateModel.php
+++ core/Models/DonateModel.php
@@ -3,14 +3,19 @@
 use Core\Conf\Kyaaaa\DB;
 
 class DonateModel {
-    public function __construct() {
-        $this->users = DB::query('users');
-    }
 
     public function get_username() {
-        $builder = $this->users;
+        $builder = DB::query('users');
         $builder->select('username');
         $query = $builder->get()[0]->username;
+        return $query;
+    }
+
+    public function get_donations() {
+        $builder = DB::query('donations');
+        $builder->select('*');
+        $builder->where('status', 'PAID');
+        $query = $builder->get();
         return $query;
     }
 
