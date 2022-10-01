1f773dfe8765c92197ae5f579300cd94bf94ad26
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 21:17:27 +0700

    Update layout.kyaaaa.php

--- core/Views/admin/layout.kyaaaa.php
+++ core/Views/admin/layout.kyaaaa.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?></title>
-    <link href="<?= url('assets/home/css') ?>/custom.css" rel="stylesheet">
+    <link href="<?= url('assets/css') ?>/custom.css" rel="stylesheet">
 </head>
 <body>
     <?= $content; ?>
-<script src="<?= url('assets/home/js') ?>/custom.js"></script>
+<script src="<?= url('assets/js') ?>/pagination.js"></script>
 </body>
 </html>
