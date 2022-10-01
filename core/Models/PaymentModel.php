0173b0678e87c795d6ec82723341c5b6ef74a94a
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 17:42:10 +0700

    Update PaymentModel.php

--- core/Models/PaymentModel.php
+++ core/Models/PaymentModel.php
@@ -11,4 +11,20 @@ class PaymentModel {
         return $query;
     }
 
+    public function get_merchant_ref($merchant_ref) {
+        $builder = DB::query('donations');
+        $builder->select('*');
+        $builder->where('merchant_ref', $merchant_ref);
+        $query = $builder->get();
+        return $query;
+    }
+
+    public function update_donation($merchant_ref, $status) {
+        $builder = DB::query('donations');
+        $builder->update(['status' => $status]);
+        $builder->where('merchant_ref', $merchant_ref);
+        $query = $builder->save();
+        return $query;
+    }
+
 }
