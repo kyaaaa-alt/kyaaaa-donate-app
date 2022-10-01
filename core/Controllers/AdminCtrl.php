af765c88909af9e82ca6746ad9e2ab39bd64b767
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 21:17:44 +0700

    Update AdminCtrl.php

--- core/Controllers/AdminCtrl.php
+++ core/Controllers/AdminCtrl.php
@@ -1,11 +1,68 @@
 <?php namespace Core\Controllers;
 
+use Core\Models\AdminModel;
+
 class AdminCtrl {
 
+    public function __construct() {
+        $this->AdminModel = new AdminModel();
+        $this->request = request();
+        $this->session = session();
+    }
+
+    public function login() {
+        $data['title'] = 'Login';
+        return view('auth/auth', $data);
+    }
+
+    public function logout() {
+        $this->session->clear();
+        redirectTo(url('login'));
+    }
+
+    public function auth() {
+        $email = $this->request->post('email');
+        $password = $this->request->post('password');
+        $get = $this->AdminModel->get_user_by_email($email);
+        if (!empty($get)) {
+            $verifyPassword = password_verify($password, $get[0]->password);
+            if ($verifyPassword) {
+                if ($get[0]->roles == '1') {
+                    $sessiondata = [
+                        'isAdmin' => TRUE,
+                        'name' => $get[0]->name,
+                        'username' => $get[0]->username,
+                        'email' => $get[0]->email,
+                    ];
+                    $this->session->set($sessiondata);
+                    return redirectTo(url('dashboard'));
+                } else {
+                    $this->session->flash('notif', 'invalid role');
+                    return redirectTo(url('login'));
+                }
+
+            } else {
+                $this->session->flash('notif', 'Password/Email Salah!');
+                return redirectTo(url('login'));
+            }
+        } else {
+            $this->session->flash('notif', 'Password/Email Salah!');
+            return redirectTo(url('login'));
+        }
+    }
+
     public function index() {
-        $data['title'] = 'Docs';
-        $data['subtitle'] = 'Sample Pages';
-        return view('home/docs', $data);
+        $data['title'] = 'Dashboard';
+        $data['donations'] = $this->AdminModel->get_donations();
+        $data['users'] = $this->AdminModel->get_users();
+        return view('admin/index', $data);
+    }
+
+    public function settings() {
+        $data['title'] = 'Settings';
+        $data['settings'] = $this->AdminModel->get_settings();
+        $data['users'] = $this->AdminModel->get_users();
+        return view('settings/index', $data);
     }
 
 }
