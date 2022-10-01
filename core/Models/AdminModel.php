611631a6652a034d1d0c7db6ce6bcd354d7054d1
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-01 21:17:39 +0700

    Update AdminModel.php

--- core/Models/AdminModel.php
+++ core/Models/AdminModel.php
@@ -2,15 +2,35 @@
 
 use Core\Conf\Kyaaaa\DB;
 
-class AdminModelModel {
-    public function __construct() {
-        $this->users = DB::query('users');
+class AdminModel {
+
+    public function get_user_by_email($email) {
+        $builder = DB::query('users');
+        $builder->select('*');
+        $builder->where('email', $email);
+        $query = $builder->get();
+        return $query;
     }
 
-    public function get_users_active() {
-        $builder = $this->users;
+    public function get_donations() {
+        $builder = DB::query('donations');
         $builder->select('*');
+        $builder->orderBy('id', 'DESC');
         $query = $builder->get();
+        return $query;
+    }
+
+    public function get_users() {
+        $builder = DB::query('users');
+        $builder->select('*');
+        $query = $builder->get()[0];
+        return $query;
+    }
+
+    public function get_settings() {
+        $builder = DB::query('settings');
+        $builder->select('*');
+        $query = $builder->get()[0];
         return $query;
     }
 
