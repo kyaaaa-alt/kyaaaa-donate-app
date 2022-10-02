40d1605acd6d03ddda9d11ca77f34a7d95076c01
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-02 23:04:12 +0700

    Create index.kyaaaa.php

---
+++ core/Views/streamoverlay/donatenotification/index.kyaaaa.php
@@ -0,0 +1,8 @@
+<div class="content">
+    <img src="<?= url('assets/img')?>/stream.gif" width="150">
+    <h1 class="thankyou">Thank You!</h1>
+    <p class="textbox">
+        <span class="highlight shake" id="amount">Rp 1.000.000.000</span> dari <span class="highlight shake" id="name">Robot</span><br>
+        <span id="msgs">Ini adalah test, notifikasi donasi siap digunakan!</span>
+    </p>
+</div>
