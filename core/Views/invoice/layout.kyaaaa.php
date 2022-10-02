1a0dc69032ecb52cfe0f2c535f1006c58296aa27
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-02 23:04:15 +0700

    Update layout.kyaaaa.php

--- core/Views/invoice/layout.kyaaaa.php
+++ core/Views/invoice/layout.kyaaaa.php
@@ -6,7 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>QRIS Payment</title>
     <link href="<?= url('assets/css') ?>/custom.css" rel="stylesheet">
-
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+    <script>
+        var pusher = new Pusher('<?= $settings->pusher_key ?>', {
+            cluster: '<?= $settings->pusher_cluster ?>'
+        });
+        var channel = pusher.subscribe('my_stream');
+        channel.bind('donate_event', function(data) {
+            var data = JSON.stringify(data);
+            let response = JSON.parse(data);
+            const currMerchantRef = '<?= $response->data->merchant_ref ?>';
+            if (response.merchant_ref == currMerchantRef) {
+                document.getElementById('box').innerHTML = '<h3>Thank You, '+response.name+'</h3><p class="text-center">Dukungan kamu sebesar '+response.amount+' berhasil terkirim!</p>';
+            }
+        });
+    </script>
 </head>
 <body>
 <?= $content; ?>
