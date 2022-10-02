43cff9ab53ca798a12a7195115910d11b5ffabb3
Naufal Azkia <66082449+naufkia@users.noreply.github.com>
2022-10-02 18:24:44 +0700

    Update Routes.php

--- core/Conf/Routes.php
+++ core/Conf/Routes.php
@@ -7,21 +7,38 @@ class Routes {
         $router = new Router();
 
         $router->get('/', [\Core\Controllers\DonateCtrl::class,'index']);
+
         $router->post('/do_donate', [\Core\Controllers\PaymentCtrl::class,'do_donate'])
             ->middleware('\Core\Filter\CSRF#donate');
+
         $router->get('/inv/:id', [\Core\Controllers\PaymentCtrl::class,'invoice']);
 
         $router->get('/login', [\Core\Controllers\AdminCtrl::class,'login']);
+
         $router->get('/logout', [\Core\Controllers\AdminCtrl::class,'logout']);
+
         $router->post('/auth', [\Core\Controllers\AdminCtrl::class,'auth'])
             ->middleware('\Core\Filter\CSRF#auth');
 
         $router->get('/dashboard', [\Core\Controllers\AdminCtrl::class,'index'])
             ->middleware('\Core\Filter\Auth#admin');
+
         $router->get('/dashboard/settings', [\Core\Controllers\AdminCtrl::class,'settings'])
             ->middleware('\Core\Filter\Auth#admin');
 
         $router->post('/update_profile', [\Core\Controllers\AdminCtrl::class,'update_profile'])
+            ->middleware('\Core\Filter\Auth#admin')
+            ->middleware('\Core\Filter\CSRF#update');
+
+        $router->post('/update_stream', [\Core\Controllers\AdminCtrl::class,'update_stream'])
+            ->middleware('\Core\Filter\Auth#admin')
+            ->middleware('\Core\Filter\CSRF#update');
+
+        $router->post('/update_tripay', [\Core\Controllers\AdminCtrl::class,'update_tripay'])
+            ->middleware('\Core\Filter\Auth#admin')
+            ->middleware('\Core\Filter\CSRF#update');
+
+        $router->post('/update_pusher', [\Core\Controllers\AdminCtrl::class,'update_pusher'])
             ->middleware('\Core\Filter\Auth#admin')
             ->middleware('\Core\Filter\CSRF#update');
 
